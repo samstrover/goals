e253f55d2b8c45e4997eadd6c68eacb7abf7b1db
samstrover <sam.strover@sammacmedia.co.zw>
2022-12-11 10:50:49 +0200

    first commit

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,35 @@
+const asyncHandler = require('express-async-handler')
+//@desc Get Goals
+//@route GET /api/goals
+//@access Private
+const getGoals = asyncHandler(async(req, res) =>{
+    res.status(200).json({message: 'Get goals'})
+})
+//@desc Set Goal
+//@route POST /api/goals
+//@access Private
+const setGoal = asyncHandler(async(req, res) =>{
+    if(!req.body.text){
+        res.status(400)
+        throw new Error('please add a text field')
+    }
+    res.status(200).json({message: 'Set goal'})
+})
+//@desc Update Goals
+//@route PUT /api/goals/id
+//@access Private
+const updateGoal = asyncHandler(async(req, res) =>{
+    res.status(200).json({message: `Update goal ${req.params.id}`})
+})
+//@desc Delete Goal
+//@route Delete /api/goals:id
+//@access Private
+const deleteGoal = asyncHandler(async(req, res) =>{
+    res.status(200).json({message: `Delete goal ${req.params.id}`})
+})
+module.exports = {
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal
+}
